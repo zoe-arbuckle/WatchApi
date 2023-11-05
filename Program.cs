e16f7f69d7d40f4c5f0aad36d416458d5f74d6a2
Zoe Arbuckle <zoe.arbuckle@me.com>
2023-11-05 14:14:15 -0500

    add discount calculation, separate calc function

--- Program.cs
+++ Program.cs
@@ -38,6 +38,8 @@ TypesOfWatches.Add(new Watch()
 app.MapPost("/checkout", (string[] ids) =>
 {
     int totalPrice = 0;
+    // dictionary key = watch id, value = number of watches in checkout
+    Dictionary<int, int> WatchCheckoutCount = new Dictionary<int, int>();
     foreach (string stringId in ids)
     {
         int id = 0;
@@ -47,15 +49,33 @@ app.MapPost("/checkout", (string[] ids) =>
             throw new ArgumentException();
         }
 
-        Watch? w = TypesOfWatches.Find(watch => watch.Id == id);
-        if(w == null){
-            throw new Exception();
+        if(WatchCheckoutCount.ContainsKey(id)){
+            WatchCheckoutCount[id] += 1;
+        } else {
+            WatchCheckoutCount[id] = 1;
         }
-        totalPrice += w.UnitPrice;
-
     }
+
+    totalPrice = CalculateTotal(WatchCheckoutCount);
+
     return new { Price = totalPrice };
 
 });
+
+int CalculateTotal(Dictionary<int, int> watchCheckoutCount)
+{
+    int total = 0;
+    foreach(var item in watchCheckoutCount){
+        Watch? w = TypesOfWatches.Find(watch => watch.Id == item.Key);
+        if(w == null){
+            throw new ArgumentNullException();
+        }
+
+        total += (item.Value/w.UnitsForDiscount) * w.DiscountMarkdown
+            + (item.Value%w.UnitsForDiscount) * w.UnitPrice;
+    }
+
+    return total;
+}
 
 app.Run();
